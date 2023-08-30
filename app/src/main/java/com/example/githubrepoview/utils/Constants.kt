ace159b618fb9d50a8ab2f5d5f4727fc3fd94503
abdelrhmanhosnyrunprof <abdelrhmanhosny.runprof@gmail.com>
2023-08-31 01:39:06 +0300

    remove token bec problem with token scope problem from my github , so i remove it and because of this reach limit decrease

--- app/src/main/java/com/example/githubrepoview/utils/Constants.kt
+++ app/src/main/java/com/example/githubrepoview/utils/Constants.kt
@@ -3,4 +3,4 @@ package com.example.githubrepoview.utils
 enum class Status { LOADING, SUCCESS, ERROR }
 
 const val BASE_URL ="https://api.github.com/"
-const val token = "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"
+const val token = ""
